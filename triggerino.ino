555fe5d8a1600fbbd88336f22b6d6503d014e96d
marcobarilari <marcobarilr@gmail.com>
2020-09-19 21:34:49 +0200

    add comments in the code

--- triggerino.ino
+++ triggerino.ino
@@ -1,25 +1,32 @@
 #include "Keyboard.h"
 
+// 
+
+// select the pin where the is the led
 int ledPin = 10;
 
 void setup() {
-  // put your setup code here, to run once:
 
+  // init keyboard
   Keyboard.begin();
+
+  // init led
   pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // print the letter that is red as the trigger by PTB
   Keyboard.print('t');
-  
+
+  // led ON
   digitalWrite(ledPin, HIGH); 
 
+  // wait for the TR time
   delay(1800); //msec
 
-    digitalWrite(ledPin, LOW); 
-
+  // led OFF
+  digitalWrite(ledPin, LOW); 
 
 }
